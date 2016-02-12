2c16f71d272300eddca63eaf17de853e79da3ef7
yesseniavalencia@csil.cs.ucsb.edu <yvalencia@csil.cs.ucsb.edu>
2016-02-12 15:36:19 -0800

    Just created the two files AnimatedPictureComponent and AnimatedPictureViewer based on jstaahl so Component was factored out into anotherclass

---
+++ src/edu/ucsb/cs56/w16/drawings/yvalencia/advanced/AnimatedPictureComponent.java
@@ -0,0 +1,111 @@
+package edu.ucsb.cs56.w16.drawings.yvalencia.advanced;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+import javax.swing.JComponent;
+
+// the four tools things we'll use to draw
+
+import java.awt.geom.Line2D;  // single lines
+import java.awt.geom.Ellipse2D;  // ellipses and circles
+import java.awt.Rectangle;  // squares and rectangles
+import java.awt.geom.GeneralPath; // combinations of lines and curves
+
+
+import java.awt.geom.Rectangle2D; // for rectangles drawing with Doubles
+
+import java.awt.Color; // class for Colors
+import java.awt.Shape; // Shape interface
+import java.awt.Stroke; // Stroke interface
+import java.awt.BasicStroke; // class that implements stroke
+
+import edu.ucsb.cs56.w16.drawings.utilities.ShapeTransforms;
+import edu.ucsb.cs56.w16.drawings.utilities.GeneralPathWrapper;
+
+
+/**
+   A component that draws an animated picture by Yessenia Valencia
+   
+   @author Yessenia Valencia
+   @version CS56, W16
+   
+*/
+
+
+public class AnimatedPictureComponent extends JComponent
+{  
+    private Shape person;
+    private double SpineHeight;
+    private double scribbleSpeed;
+    private double amplitude = 3.2;
+    private double travelSpeed;
+    private double xTravel = 0;
+    private double startingPosition;
+    private double startingX;
+    private double xPos;
+    private double startingY;
+    private double width;
+    private double startingLength;
+    private double t;
+    private double angle = 1.75*Math.PI;
+    private double travelDistance;
+
+
+    // starting length: 300; width: 30
+    /** Constructs an AnimatedPictureComponent with specific properties.
+	This animated picture depicts a person writing across the screen
+	@param startingX the starting x position of the person
+	@param startingY the starting y position of the person
+	@param travelSpeed the speed at which the person will move
+	across the screen
+	@param travelDistance the number of pixels the person will move
+	across the screen before stopping
+	@param scribbleSpeed the speed at which the person oscillates (or
+	scribbles) back and forth
+	@param startingLength the starting length of the person in pixels
+	@param width the width of the person in pixels
+    */
+    public AnimatedPictureComponent(double startingX, double startingY, double travelSpeed, double travelDistance, double scribbleSpeed, double startingLength, double width) {
+	this.startingX = startingX;
+	this.startingY = startingY;
+	this.xPos = startingX;
+	this.travelSpeed = travelSpeed;
+	this.travelDistance = travelDistance;
+	this.scribbleSpeed = scribbleSpeed;
+	this.startingLength = startingLength;
+	this.width = width;
+
+	person = new PersonWithCube(this.xPos, this.startingY, this.width, this.startingLength);
+	SpineHeight = ((Person)person).getSpineHeight();
+    }
+
+    /** The paintComponent method is orverriden to display
+	out animation. Each time this method is called, the
+	position of the person is updated
+    */
+    
+    public void paintComponent(Graphics g)
+    {  
+	Graphics2D g2 = (Graphics2D) g;
+	//g2.draw(new Rectangle(0, 0, 
+	if (xPos >= startingX + travelDistance) {
+	    this.xPos = startingX;
+	    this.t = 0;
+	    this.xTravel = 0;
+	    return;
+	}
+	else
+	    g2.setColor(Color.BLACK); g2.draw(person);
+
+	t += scribbleSpeed;
+	xTravel += travelSpeed;
+	xPos = xTravel + startingX;
+	double wobble = amplitude*(1/scribbleSpeed)*Math.sin(t) + amplitude*0.8*(1/scribbleSpeed)*Math.sin(0.8*t+1.5);
+	xPos += wobble;
+	double length = (1 - (xTravel/travelDistance))*(startingLength - SpineHeight) + SpineHeight;
+	double yPos = startingYPos + (startingLength - length);
+       
+	person = ShapeTransforms.rotatedCopyOf(new PersonWithCube(xPos, yPos, width, length), 0.015*wobble);
+    }    
+  
+}
